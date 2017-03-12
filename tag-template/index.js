05c1e35a703d87b20f00a80bcdfbeacffe395b0c
mrcode <734202639@qq.com>
2017-03-13 04:02:38 +0800

    taggle template

---
+++ tag-template/index.js
@@ -0,0 +1,22 @@
+
+var sender = '<>'
+var message =
+  SaferHTML`<p>${sender} has sent you a message.</p>`;
+
+console.log(message)
+
+function SaferHTML(templateData) {
+  var s = templateData[0];
+  for (var i = 1; i < arguments.length; i++) {
+    var arg = String(arguments[i]);
+
+    // Escape special characters in the substitution.
+    s += arg.replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;");
+
+    // Don't escape special characters in the template.
+    s += templateData[i];
+  }
+  return s;
+}
